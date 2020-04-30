9603af61dad6781e0ff6a57c0368c94c895101d4
LavenderJia <37802547+LavenderJia@users.noreply.github.com>
2020-04-30 14:02:17 +0800

    reconstruct code

--- LogitRegr.py
+++ LogitRegr.py
@@ -7,11 +7,11 @@ from sklearn.model_selection import StratifiedKFold
 from scipy import interp
 from collections import Counter
 
-
+# split feature and target
 def split_X_y(df, X_cols, y_cols):
     return df.loc[:,X_cols].values, df.loc[:,y_cols].values
 
-
+# data preparation
 def get_data():
     train = pd.read_csv(r'data/train.csv', parse_dates=[0], dtype={'tag': 'int64'})
     test = pd.read_csv(r'data/test.csv', parse_dates=[0], dtype={'tag': 'int64'})
@@ -29,68 +29,69 @@ def get_data():
     return train_X, train_y, test_X, test_y
 
 
-train_X, train_y, test_X, test_y = get_data()
+def train_model(clf, cv_data, test_X, test_y):
+    """
+    train and estimate model
+    draw ROC curves of train and test
+    :param clf: the model for classifier
+    :param cv: train data splited by StratifiedKFold
+    :return: the trained model
+    """
+
+    tprs = []  # list for saving TP rates in each cv
+    aucs = []  # list for saving aucs in each cv
+    mean_fpr = np.linspace(0, 1, 100)  # mean FP rates
+    fig, ax = plt.subplots()
+    for i, (train, valid) in enumerate(cv_data): # 5 fold training of model
+        clf.fit(train_X[train], train_y[train])
+        # plot ROC
+        viz = metrics.plot_roc_curve(clf, train_X[valid], train_y[valid], name='ROC fold {}'.format(i), alpha=0.3, lw=1, ax=ax)
+        interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)  # get TP rates
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+    # plot ROC of test data
+    metrics.plot_roc_curve(clf, test_X, test_y, name='ROC test', alpha=0.8, lw=1, color='green', ax=ax)
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
+    # draw mean auc of 5 cv train
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = metrics.auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC of Train (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+    # draw confident interval
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+           title="ROC Curve")
+    ax.legend(loc="lower right")
+    plt.savefig('res_fig/LogitRegrRes.png')
+    return clf
 
 
-# create cross-validation instance
-cv = StratifiedKFold(n_splits=5)
-# create model
-clf = LogisticRegression(solver='sag', max_iter=500, class_weight='balanced', random_state=0)
-
-tprs = []
-aucs = []
-mean_fpr = np.linspace(0, 1, 100)
-fig, ax = plt.subplots()
-for i, (train, valid) in enumerate(cv.split(train_X, train_y)):
-    clf.fit(train_X[train], train_y[train])
-    viz = metrics.plot_roc_curve(clf, train_X[valid], train_y[valid],
-                         name='ROC fold {}'.format(i),
-                         alpha=0.3, lw=1, ax=ax)
-    interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)
-    interp_tpr[0] = 0.0
-    tprs.append(interp_tpr)
-    aucs.append(viz.roc_auc)
-
-viz = metrics.plot_roc_curve(clf, test_X, test_y,
-                         name='ROC test',
-                         alpha=0.8, lw=1, color='green', ax=ax)
-
-ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-        label='Chance', alpha=.8)
-
-mean_tpr = np.mean(tprs, axis=0)
-mean_tpr[-1] = 1.0
-mean_auc = metrics.auc(mean_fpr, mean_tpr)
-std_auc = np.std(aucs)
-ax.plot(mean_fpr, mean_tpr, color='b',
-        label=r'Mean ROC of Train (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-        lw=2, alpha=.8)
+# calculate test score
+def test_score(pred_y, test_y):
+    y_pred_test = np.array([pred_y, test_y]).T
+    # TP:0, FN:1, FP: 2, TN:3
+    y_pred_test_count = Counter(np.dot(y_pred_test, np.array([1,2])))
+    # normal: 0, ice:1
+    y_test_count = Counter(test_y)
+    score = (1 - 0.5 * (y_pred_test_count.get(1)/y_test_count.get(0)) - 0.5 * (y_pred_test_count.get(2)/y_test_count.get(1))) * 100
+    print("The final test score is {:.2f}".format(score))
 
 
-std_tpr = np.std(tprs, axis=0)
-tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                label=r'$\pm$ 1 std. dev.')
-
-ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-       title="ROC Curve")
-ax.legend(loc="lower right")
-plt.show()
-
-# test_score = clf.score(test_X, test_y)
-# print('The score in test data is ' + str(test_score))
-
-# calculate score
-y_pred = clf.predict(test_X)
-y_pred_test = np.array([y_pred, test_y]).T
-# TP:0, FN:1, FP: 2, TN:3
-y_pred_test_count = Counter(np.dot(y_pred_test, np.array([1,2])))
-# normal: 0, ice:1
-y_test_count = Counter(test_y)
-
-# calculate score
-score = (1 - 0.5 * (y_pred_test_count.get(1)/ y_test_count.get(0)) - 0.5 * (y_pred_test_count.get(2)/ y_test_count.get(1))) * 100
-
-print("The final test score is {:.2f}".format(score))
-
+if __name__ == '__main__':
+    train_X, train_y, test_X, test_y = get_data()
+    # create cross-validation instance
+    cv = StratifiedKFold(n_splits=5)
+    cv_data = cv.split(train_X, train_y)
+    # create model
+    clf = LogisticRegression(solver='sag', max_iter=500, class_weight='balanced', random_state=0)
+    clf = train_model(clf, cv_data, test_X, test_y)
+    pred_y = clf.predict(test_X)
+    test_score(pred_y, test_y)
